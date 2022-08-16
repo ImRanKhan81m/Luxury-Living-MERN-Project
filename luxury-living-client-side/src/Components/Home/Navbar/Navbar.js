54d6764d1554dd53ba500c1ed5763c528b869349
Md Imran Hossen <freelancerimran81@gmail.com>
2022-08-16 10:06:52 +0600

    navbar section updated

--- luxury-living-client-side/src/Components/Home/Navbar/Navbar.js
+++ luxury-living-client-side/src/Components/Home/Navbar/Navbar.js
@@ -12,8 +12,10 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link to='/'>Home</Link></li>
-                            <li><a>Item 3</a></li>
+                            <li className='mb-2 px-0'><Link to='/'>Home</Link></li>
+                            <li className='mb-2 px-0'><Link to='/'>Projects</Link></li>
+                            <li className='mb-2 px-0'><Link to='/'>About Us</Link></li>
+                            <li className='mb-2 px-0'><Link to='/'>Contact Us</Link></li>
                         </ul>
                     </div>
                     <img className='lg:w-32 md:w-28 sm:w-24 w-20' src={logo} alt="" />
@@ -22,8 +24,8 @@ const Navbar = () => {
                     <div class="navbar-center hidden lg:flex">
                         <ul class="menu menu-horizontal p-0">
                             <li className='mr-2 px-0'><Link to='/'>Home</Link></li>
-                            <li className='mr-2 px-0'><Link to='/'>Our Portfolio</Link></li>
-                            <li className='mr-2 px-0'><Link to='/'>Our Team</Link></li>
+                            <li className='mr-2 px-0'><Link to='/'>Projects</Link></li>
+                            <li className='mr-2 px-0'><Link to='/'>About Us</Link></li>
                             <li className='mr-2 px-0'><Link to='/'>Contact Us</Link></li>
                         </ul>
 
--- luxury-living-client-side/src/Components/Home/Services/Services.js
+++ luxury-living-client-side/src/Components/Home/Services/Services.js
@@ -4,7 +4,8 @@ import Service from './Service';
 const Services = () => {
     return (
         <div className='mid-container'>
-            <h1 className='text-4xl font-bold text-center lg:mt-24 md:mt-16 my-10'>Our Awesome <span className='text-primary'>Services</span></h1>
+            <h1 className='text-4xl font-bold text-center lg:mt-24 md:mt-16 my-10'>We're an agency tailored to all <br/> 
+                clients' needs that always delivers</h1>
 
             <div className=' lg:my-16 md:my-10'>
                 <Service />
